b08bf29151e872d72ec2904c4c3b776a8c9c94c2
Andrei Ionescu <andrei.ionescu@griffithuni.edu.au>
2018-09-03 09:40:23 +1000

    Added more to the JSON database, started Channel functionality

--- ChatProject/ChatServer/routes/api.js
+++ ChatProject/ChatServer/routes/api.js
@@ -10,7 +10,7 @@ module.exports = function(app, fs) {
                 Database = JSON.parse(data);
                 Users = Database[0].Users;
                 for (let i = 0; i < Users.length; i++) {
-                    if (Users[i].username === uname) {
+                    if (Users[i].Username === uname) {
                         res.send({'UserData': JSON.stringify(Users[i]), 'success': true});
                         return;
                     }
@@ -21,6 +21,23 @@ module.exports = function(app, fs) {
     });
 
     app.post('/api/GroupData', (req, res) => {
-
+        var groupName = req.body.groupName;
+        var Groups;
+        fs.readFile('routes/Users.json', 'utf8', function(err, data) {
+            if (err) {
+                console.log(err);
+                res.send({'GroupData': '', 'success': false});
+            } else {
+                Database = JSON.parse(data);
+                Groups = Database[0].Groups;
+                for (let i = 0; i < Groups.length; i++) {
+                    if (Groups[i].Group === groupName) {
+                        res.send({'GroupData': JSON.stringify(Groups[i]), 'success': true});
+                        return;
+                    }
+                }
+                res.send({'GroupData': 'NotFound', 'success': false});
+            }
+        })
     });
 }
--- ChatProject/src/app/group/group.component.ts
+++ ChatProject/src/app/group/group.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+export interface GroupData {
+  channels: string;
+  success: boolean;
+}
 
 @Component({
   selector: 'app-group',
@@ -9,9 +16,26 @@ export class GroupComponent implements OnInit {
   @Input('groupName') groupName: string;
   isCollapsed = false;
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.getGroupData();
+  }
+
+  GetGroup(group: string) {
+    return this.http.post<GroupData>('/api/GroupData', {groupName: group});
+  }
+
+  getGroupData() {
+    this.GetGroup(this.groupName).subscribe(
+      data => {
+        if (data.success) {
+          console.log('GotHere 4');
+        }
+      },
+      error => {
+        alert('failed');
+      });
   }
 
 }
